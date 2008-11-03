98c9cff6924bc78fb653a0e8156019b486cfd61b
Chris Forno <chris@forno.us>
2008-11-03 10:01:04 -0800

    Adding CSS and JavaScript files.
    darcs-hash:20081103180104-0a35a-781adfee46e23f2d0f151b6677625ce92cc1daea.gz

---
+++ vocabulink.com/s/review.js
@@ -0,0 +1,16 @@
+addLoadEvent(setupSignals);
+
+function setupSignals () {
+    var lexemeCover = $('lexeme-cover');
+    var hiddenLexeme = getNodeAttribute($('hidden-lexeme'), 'value');
+    var startTime = new Date();
+    connect(lexemeCover, 'onclick', partial(showLexeme, lexemeCover, hiddenLexeme, startTime));
+}
+
+function showLexeme (lexemeCover, hiddenLexeme, startTime) {
+    var stopTime = new Date();
+    var recallTime = stopTime.getTime() - startTime.getTime();
+    swapDOM(lexemeCover, SPAN({'class': 'lexeme'}, hiddenLexeme));
+    setNodeAttribute($('recall-time'), 'value', recallTime);
+    stop();
+}
