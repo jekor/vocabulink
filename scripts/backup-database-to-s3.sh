e9830b2a93ea62f4a95a16145e3c5f27e8272133
Chris Forno <jekor@jekor.com>
2012-10-14 22:45:18 -0700

    I don't want to have to setup a private key on a server just to sign my own key.

--- scripts/backup-database-to-s3.sh
+++ scripts/backup-database-to-s3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 FILE=vocabulink--$(date +%Y-%m-%d).sql.gpg
-pg_dump -U postgres -h localhost --create vocabulink | gpg -e -r "chris@forno.us" > $FILE
+pg_dump -U postgres -h localhost --create vocabulink | gpg -e -r "chris@forno.us" --trust-model always > $FILE
 s3cmd put $FILE s3://vocabulink.com-archive/sql/
 rm $FILE
